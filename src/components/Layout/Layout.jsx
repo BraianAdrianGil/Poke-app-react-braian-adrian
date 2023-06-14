3aa0fa15ba0e4090f019f59e28b6a389447b6576
BraianAdrian <braian.adrian.gagliardo@gmail.com>
2023-06-14 13:43:15 -0300

    added several styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom";
 import HeaderCommon from "../common/HeaderCommon/HeaderCommon";
-
 const Layout = () => {
   return (
     <div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,17 +17,50 @@ const Home = () => {
       <div className="pokedex__img__container">
         <div className="pokeball__img"></div>
       </div>
-
       <div className="pikachu__container">
         <img src="https://i.imgur.com/tV5jP6X.png" alt="" />
       </div>
-
       <div className="welcome__general__container">
         <h1>¡Hi Trainer!</h1>
         <p>To begin, please give me your name</p>
       </div>
-
+      <div className="home__footer__social__networks">
+        <a
+          href="https://github.com/BraianAdrianGil/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="bx bxl-github"></i>
+        </a>
+        <a
+          href="https://uy.linkedin.com/in/braian-adrian-gil-a10042266"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i
+            className="bx bxl-linkedin-square"
+            style={{ color: "#227fb9" }}
+          ></i>
+        </a>
+        <a href="https://twitter.com/" target="_blank" rel="noreferrer">
+          <i className="bx bxl-twitter" style={{ color: "#6dd4d4" }}></i>
+        </a>
+      </div>
       <UserNameForm onSendName={handleSendName} />
+      <footer className="home__footer__general__container">
+        <div className="home__footer__rights__general__container">
+          <p>&copy; 2023 Braian Adrian G. Todos los derechos reservados.</p>
+        </div>
+        <div className="home__footer__contact__general__container">
+          <p>
+            <a href="mailto:braian.adrian.gagliardo@gmail.com">
+              braian.adrian.gagliardo@gmail.com
+            </a>
+            <span> | </span>
+            <a href="tel:+598098604405"> +598098604405</a>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 };
